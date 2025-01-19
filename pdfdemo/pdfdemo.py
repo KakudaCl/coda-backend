4ec79701239484df15dceba33ce2b60dbc984fbe
角田 直樹 <kakuda@iret.co.jp>
2025-01-20 08:17:12 +0900

    feat: 譜面データに四角形を描画することに成功

--- pdfdemo/pdfdemo.py
+++ pdfdemo/pdfdemo.py
@@ -1,10 +1,17 @@
-from PyPDF4 import PdfFileReader
-from PIL import Image
+import pdf2image
+import cv2 as cv
+import numpy as np
 
-# 画像抽出用PDFを取得
-pdf = open("etude1-4.pdf", "rb")
-reader = PdfFileReader(pdf)
+def main():
+    # 画像抽出用PDFを取得
+    print("Hello")
+    pdfimages = pdf2image.convert_from_path("etude1-4.pdf")
+    cvimage = np.asarray(pdfimages[0]).copy()
 
-# ページ数を取得
-page_count = reader.getNumPages()
-print(page_count)
+    thresh = cv.rectangle(cvimage,(384,0),(510,128),(0,255,0),3)
+    # ret, thresh = cv.threshold(cvimage, 127,255, cv.THRESH_BINARY_INV)
+    cv.imshow("pdf2image sample", thresh)
+    cv.waitKey(0)
+
+if __name__ == '__main__':
+    main()
