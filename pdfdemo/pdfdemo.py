cd452fd46704c946755f25bc36b21ae95ebd4609
角田 直樹 <kakuda@iret.co.jp>
2025-01-21 13:44:11 +0900

    feat: 画像処理後のデータ出力部分を追加

--- pdfdemo/pdfdemo.py
+++ pdfdemo/pdfdemo.py
@@ -1,6 +1,8 @@
 import pdf2image
 import cv2 as cv
 import numpy as np
+from PIL import Image
+from reportlab.pdfgen import canvas
 
 def main():
     # 画像抽出用PDFを取得
@@ -8,10 +10,22 @@ def main():
     pdfimages = pdf2image.convert_from_path("etude1-4.pdf")
     cvimage = np.asarray(pdfimages[0]).copy()
 
-    thresh = cv.rectangle(cvimage,(384,0),(510,128),(0,255,0),3)
+    font = cv.FONT_HERSHEY_SIMPLEX
+    thresh = cv.putText(cvimage,'OpenCV',(150,150), font, 4,(0,255,0),2,cv.LINE_AA)
     # ret, thresh = cv.threshold(cvimage, 127,255, cv.THRESH_BINARY_INV)
     cv.imshow("pdf2image sample", thresh)
     cv.waitKey(0)
+
+    cv.imwrite("output.png", cvimage)
+
+    image = Image.open("output.png")
+    width, height = image.size
+
+    c = canvas.Canvas("outputpdf.pdf", pagesize=(width, height))
+
+    c.drawImage("output.png", 0, 0, width, height)
+    c.showPage()
+    c.save()
 
 if __name__ == '__main__':
     main()
