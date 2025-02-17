e474ea42824f5833c0c3992756eb408266b58963
角田 直樹 <kakuda@iret.co.jp>
2025-02-17 20:35:54 +0900

    fix: フロント側からAPIを呼び出せるようにCORS周りを設定

--- coda-fastapi/api/main.py
+++ coda-fastapi/api/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
-from api.routers import task, done
+from starlette.middleware.cors import CORSMiddleware
+
+from api.routers import done, pdf_edit, task
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,   # 追記により追加
+    allow_methods=["*"],      # 追記により追加
+    allow_headers=["*"]       # 追記により追加
+)
+
 app.include_router(task.router)
 app.include_router(done.router)
+app.include_router(pdf_edit.router)
 
 
 @app.get("/hello", tags=["FastAPI学習"])
